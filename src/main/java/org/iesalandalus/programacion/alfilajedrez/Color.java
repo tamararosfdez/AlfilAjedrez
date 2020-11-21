fe84cd2a7ba933f14c522f8dab6c849764e5097b
Tamara <Tamara@LAPTOP-4G2I14JP>
2020-11-21 18:06:39 +0100

    Añadir direcciones

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Color.java
@@ -0,0 +1,5 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+public enum Color {
+	BLANCO, NEGRO;
+}
---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Direccion.java
@@ -0,0 +1,5 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+public enum Direccion {
+	ARRIBA_DERECHA, ABAJO_DERECHA, ABAJO_IZQUIERDA, ARRIBA_IZQUIERDA;
+}
