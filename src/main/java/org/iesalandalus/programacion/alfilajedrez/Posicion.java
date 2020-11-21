7a722354c737dff236a0c55850a0a08ca0ea8245
Tamara <Tamara@LAPTOP-4G2I14JP>
2020-11-21 19:35:56 +0100

    Métodos Get y Set, y creacion de un constructor

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -4,5 +4,32 @@ public class Posicion {
 	
 	private int fila;
 	private char columna;
+	
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+	
+	public int getFila() {
+		return fila;
+	}
+	
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+			}
+		this.fila = fila;
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+	
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+			}
+		this.columna = columna;
+	}
 
 }
