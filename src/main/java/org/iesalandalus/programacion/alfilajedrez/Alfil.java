90b58db68671f8296329003957e6d0b6a4e23798
Tamara <Tamara@LAPTOP-4G2I14JP>
2020-11-22 14:46:35 +0100

    Constructor por defecto

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -1,7 +1,36 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
 public class Alfil {
+
 	private Color color;
 	private Posicion posicion;
+	
+	
+	
+	public Alfil(Color color, Posicion posicion) {
+		super();
+		this.color = color;
+		this.posicion = posicion;
+	}
+
+
+	public Color getColor() {
+		return color;
+	}
+
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 	
 }
