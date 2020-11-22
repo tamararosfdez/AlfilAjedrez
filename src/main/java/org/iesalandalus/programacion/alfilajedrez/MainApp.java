f17631f7b7ce9717d3ae9a211f6ff8b889d0a958
Tamara <Tamara@LAPTOP-4G2I14JP>
2020-11-22 20:14:15 +0100

    -	void mostrarAlfil():

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -0,0 +1,12 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		char[] Alfil = null;
+			
+			System.out.print(Alfil);
+
+	}
+
+}
